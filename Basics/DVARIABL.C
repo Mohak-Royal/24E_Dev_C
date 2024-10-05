2f8f55b1d8633985c0dc1bc737f4f73d4cd62ab0
Mohak-Royal <mohak.royal2112@gmail.com>
2024-10-05 12:46:11 +0530

    Variable , DataTypes , FormatSpecifiers

---
+++ Basics/DVARIABL.C
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+void main(){
+	//int num; // declaration
+	int num = 101; // declaration and initialization
+	clrscr();
+
+	//num = 11;  // initialization
+
+	printf("num = %d",num);
+
+
+
+	getch();
+}
+
+
+
+
+
+/*
+
+Data Types , Variables , Format Specifiers
+
+
+
+1. DataTypes :
+	- Integer : numbers without point or fractional values : int
+		int range : -32768 * 32767
+	- decimal : numbers with fractional or point values : float
+		float range : 6 decimal point
+	- character : alphabets , int , float , symbols(+,*,#) : char
+		char range : -128 to 127 /  0 to 255
+	- double : numbers with point values but with increased capacity : double
+		double range : 15 decimal point
+
+
+2. Format Specifiers: it specifies the type of the data
+
+	int : %d
+	float : %f
+	char : %c
+	double : %lf
+
+
+
+3. Variable : it is a named location of a memory address .
+	      - it is used to store the data.
+
+	      Syntax:
+
+	      Data_type  variable_name;  // declaration
+
+
+	      variable_name = value;  // initialization
+
+
+	      Data_type variable_name = value; // declaration / initailization
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
