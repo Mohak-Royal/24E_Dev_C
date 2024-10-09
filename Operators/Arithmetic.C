73d175871a380a7116d34638bf07c01399d6889b
Mohak-Royal <mohak.royal2112@gmail.com>
2024-10-09 12:40:41 +0530

    Arithmetic Operators

---
+++ Operators/Arithmetic.C
@@ -0,0 +1,91 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+void main(){
+	int a = 10 , b = 5 , ans;
+	clrscr();
+
+	//  +
+
+	//ans = a+b;
+	//printf("%d" , ans);
+
+	//printf("%d" , a+b);
+
+
+	// -
+
+	//printf("%d" , a-b);
+
+	//  *
+
+	//printf("%d" , a * b);
+
+	//   /
+
+	//printf("%d" , a / b);
+
+	// %
+
+	//printf("%d" , a%b);
+
+
+	//ans = (20 + 2) * 3;
+
+	//printf("%d" , ans);
+
+
+
+	getch();
+}
+
+
+/*
+Operators:  operators are the symbols that are used to perform certain
+	    operations like : addition , mulitplication , comparision.
+
+
+1.Arithmetic Operators
+2.Relational Operators
+3.Logical Operators
+4.Bitwise Operators
+5.Unary Operators
+6.Ternary Operators
+
+
+
+** Arithmetic Operators **
+
+- these are the type of operators which are used to perform mathematical
+  operations.
+
+:- Addition :   +  : it will add/sum the two operands
+     Syntax:    operand1  +  operand2
+
+:- Subtraction :  -  : it will minus/subtract/difference the two operands
+     Syntax:    operand1  -  operand2
+
+:- Multiplication :  *  : it will multiply/product the two operands
+     Syntax:    operand1  *  operand2
+
+:- Division : /(forward) : it will divide the two operands.
+			   it return quotiont.
+    Syntax:   opreand1 / operand2
+
+:- Modulus : %  : it will divide the two operands.
+		  it will return remainder.
+
+	Syntax:  operand1%operand2
+
+
+precedence :   + , - , * , / , %
+
+
+
+1.  * , / , % , + , -
+
+
+*/
+
+
